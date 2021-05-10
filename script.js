5a213abccb596b0d188b19cf21e20f6274f32ad4
Rewa Patil <>
2021-05-10 15:06:55 +0000

    remove trailing spaces

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 // connect "Verify your age" button to "myFunction" function
 document.getElementById('verify').addEventListener('click', myFunction)
 
-function myFunction () {
+function myFunction() {
   // get entnum and convert to integer
   const entnum = parseInt(document.getElementById('entnum').value)
 
   // check if entnum is more than 13
   if (entnum >= 13) {
     ('console.log').innerHTML = 'You can watch PG-13 movies alone'
-  }
-  else if (entnum >=17) {
+  } else if (entnum >= 17) {
     ('console.log').innerHTML = 'You can atch R-rated movies alone'
-  }
-  else if (entnum >=5) {
-   ('console.log').innerHTML = 'You can watch G or PG rated movies alone'
-  }
-  else {
+  } else if (entnum >= 5) {
+    ('console.log').innerHTML = 'You can watch G or PG rated movies alone'
+  } else {
     ('console.log').innerHTML = 'Unfortunately, you cannot watch movies alone'
-  } 
+  }
 }
